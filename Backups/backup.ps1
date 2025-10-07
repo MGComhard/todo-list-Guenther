abab7866147e1e344f08090369cfb6b2fb441149
MGComhard <MGuenther@o365.comhard.de>
2025-10-07 14:47:47 +0200

    Pfad relativ gestaltet

--- Backups/backup.ps1
+++ Backups/backup.ps1
@@ -4,14 +4,13 @@ $timestamp = Get-Date -Format "yyyy-MM-dd_HH-mm-ss"
 $backupFile = "todo_list_$timestamp.sql"
 
 # Zielordner
-$backupFolder = "backups"
-if (!(Test-Path $backupFolder)) {
-    New-Item -ItemType Directory -Path $backupFolder
-}
+$backupFolder = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+# Pfad zur Datenbank (eine Ebene höher, im Ordner "backend")
+$databasePath = Join-Path $backupFolder "..\backend\database.db"
 
 # Dump ausführen
-sqlite3.exe .\database.db ".dump" > "$backupFolder\$backupFile"
-
+sqlite3.exe $databasePath ".dump" > (Join-Path $backupFolder $backupFile)
 
 
 # Ausführen mit .\backup.ps1
